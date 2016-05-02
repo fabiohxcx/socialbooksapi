697469c7ac0c9962443ba3706b4320bc87013af3
Fabio Hideki <hideki.fabio@gmail.com>
2016-05-01 22:57:12 -0300

    adicionando Cache

--- src/main/java/com/fabiohideki/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/fabiohideki/socialbooks/resources/LivrosResources.java
@@ -2,10 +2,12 @@ package com.fabiohideki.socialbooks.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,7 +45,10 @@ public class LivrosResources {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
 		Livro livro = livrosService.buscar(id);
-		return ResponseEntity.status(HttpStatus.OK).body(livro);
+
+		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
+
+		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livro);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
